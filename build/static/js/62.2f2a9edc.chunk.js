dc14de1c7223b3499b7e1d9f78e198c39c576d36
niyongaboemmy <emmanuelniyongabo44@gmail.com>
2022-03-25 09:05:08 +0200

    another versss

---
+++ build/static/js/62.2f2a9edc.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-redux-typescript-advance"]=this["webpackJsonpreact-redux-typescript-advance"]||[]).push([[62],{1293:function(t,e,n){"use strict";n.r(e),n.d(e,"Marking",(function(){return s}));var r=n(25),c=n(24),a=n(27),i=n(26),u=n(4),p=n(1),s=function(t){Object(a.a)(n,t);var e=Object(i.a)(n);function n(){return Object(r.a)(this,n),e.apply(this,arguments)}return Object(c.a)(n,[{key:"render",value:function(){return Object(p.jsx)("div",{children:"Marking"})}}]),n}(u.Component);e.default=s}}]);
+//# sourceMappingURL=62.2f2a9edc.chunk.js.map
